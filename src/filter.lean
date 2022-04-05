de3e521a2f4f7ec720c98ffe6287f229d5c797c8
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-04-05 15:48:59 +0100

    bounded monotone has limit

---
+++ src/filter.lean
@@ -0,0 +1,103 @@
+import data.real.basic
+import order.filter.at_top_bot
+import order.liminf_limsup
+import topology.metric_space.basic
+
+open_locale filter topological_space
+
+theorem real.exists_is_glb (S : set ℝ) (hne : S.nonempty) (hbdd : bdd_below S) :
+  ∃ x, is_glb S x :=
+begin
+  set T := - S with hT,
+  have hT_ne : T.nonempty := set.nonempty_neg.mpr hne,
+  have hT_bdd : bdd_above T := bdd_above_neg.mpr hbdd,
+  use -classical.some (real.exists_is_lub T hT_ne hT_bdd),
+  simpa [← is_lub_neg] using (classical.some_spec (real.exists_is_lub T hT_ne hT_bdd)),
+end
+
+/- noncomputable lemma filter.tendsto_of_is_bounded_monotone (f : ℕ → ℝ) (h_mon : monotone f) 
+  (h_bdd : bdd_above (f '' (set.univ))) :
+  filter.tendsto f filter.at_top (𝓝 (filter.limsup (filter.at_top : filter ℕ) f)) :=
+  --filter.tendsto f (𝓝 (filter.limsup (filter.at_top : filter ℕ) f)) :=
+begin
+  have h_ne : set.nonempty (f '' (set.univ)) := sorry,
+  let B := classical.some (real.exists_is_lub (f '' (set.univ)) h_ne h_bdd),
+  have hB := classical.some_spec (real.exists_is_lub (f '' (set.univ)) h_ne h_bdd),
+  rw metric.tendsto_at_top,
+  use B,
+  rw metric.tendsto_at_top,
+  intros ε hε,
+  have hN : ∃ N : ℕ, B - ε < f N := sorry,
+  cases hN with N hN,
+  use N,
+  intros n hn,
+  simp only [dist],
+  rw abs_lt,
+  split,
+  { linarith [h_mon hn], },
+  { refine lt_of_le_of_lt _ (gt_iff_lt.mp hε),
+    rw [sub_nonpos],
+    sorry
+    }
+end -/
+
+lemma le_of_is_lub {α : Type*} [preorder α] (S : set α) (B : α) (hB : is_lub S B) :
+  ∀ s ∈ S, s ≤ B := 
+begin
+  intros s hs,
+  simp [is_lub, is_least, mem_upper_bounds] at hB,
+  exact hB.1 s hs,
+end
+
+lemma filter.tendsto_of_is_bounded_monotone (f : ℕ → ℝ) (h_mon : monotone f) 
+  (h_bdd : bdd_above (set.range f)) :
+  ∃ r : ℝ, filter.tendsto f filter.at_top (𝓝 r) :=
+begin
+  obtain ⟨B, hB⟩ := (real.exists_is_lub ((set.range f)) (set.range_nonempty f) h_bdd),
+  use B,
+  rw metric.tendsto_at_top,
+  intros ε hε,
+  have hN : ∃ N : ℕ, B - ε < f N,
+  { by_contra' h_contr,
+    have h_bound : (B - ε) ∈ upper_bounds (set.range f) ,
+    { rw mem_upper_bounds,
+      intros x hx,
+      cases (set.mem_range.mpr hx) with n hn,
+      rw ← hn,
+      exact h_contr n, },
+    rw ← (is_lub_iff_le_iff.mp hB) (B - ε) at h_bound,
+    linarith,},
+  cases hN with N hN,
+  use N,
+  intros n hn,
+  simp only [dist, abs_lt],
+  refine ⟨by linarith [h_mon hn], lt_of_le_of_lt _ (gt_iff_lt.mp hε)⟩,
+  { rw [sub_nonpos],
+    apply le_of_is_lub ((set.range f)) B hB,
+    simp only [set.image_univ, set.mem_range_self], }
+end
+
+lemma antitone.neg {α β : Type*} [preorder α] [preorder β] [add_group β]
+  [covariant_class β β (+) (preorder.to_has_le β).le]
+  [covariant_class  β β (function.swap (+)) (preorder.to_has_le β).le] {f : α → β}
+  (h_ant : antitone f) : monotone (-f) :=
+λ x y hxy, by simpa [pi.neg_apply, neg_le_neg_iff] using h_ant hxy
+
+lemma set.range_neg {α β : Type*} [add_group β] (f : α → β) :
+  set.range (-f) = - (set.range f) :=
+begin
+  ext x,
+  simp only [set.mem_range, pi.neg_apply, set.mem_neg],
+  split; rintro ⟨y, hy⟩; use y,
+  exacts [eq_neg_iff_eq_neg.mpr (eq.symm hy), (neg_eq_iff_neg_eq.mpr (eq.symm hy))],
+end
+
+lemma filter.tendsto_of_is_bounded_antitone (f : ℕ → ℝ) (h_ant : antitone f) 
+  (h_bdd : bdd_below (set.range f)) :
+  ∃ r : ℝ, filter.tendsto f filter.at_top (𝓝 r) :=
+begin
+  have h_bdd_ab : bdd_above (set.range (-f)),
+  { simpa [set.range_neg f, bdd_above_neg] using h_bdd },
+  obtain ⟨r, hr⟩ := filter.tendsto_of_is_bounded_monotone (-f) (antitone.neg h_ant) h_bdd_ab,
+  exact ⟨-r, by simpa [pi.neg_apply, neg_neg] using (filter.tendsto.neg hr)⟩,
+end
--- src/rank_one_valuation.lean
+++ src/rank_one_valuation.lean
@@ -10,15 +10,23 @@ open function multiplicative
 
 variables {R : Type*} [ring R] {Γ₀ : Type*} [linear_ordered_comm_group_with_zero Γ₀]
 
-structure is_rank_one (v : valuation R Γ₀) : Prop :=
+class is_rank_one (v : valuation R Γ₀) : Prop :=
 (rank_le_one : ∃ f : Γ₀ →* multiplicative (order_dual (with_top ℝ)), strict_mono f) --(rank_le_one : ∃ f : Γ₀ →* nnreal, strict_mono f)
 (nontrivial : ∃ r : R, v r ≠ 0 ∧ v r ≠ 1)
+
+def is_rank_one_hom (v : valuation R Γ₀) [hv : is_rank_one v] :
+  Γ₀ →* multiplicative (order_dual (with_top ℝ)) :=
+classical.some hv.rank_le_one
+
+lemma is_rank_one_strict_mono (v : valuation R Γ₀) [hv : is_rank_one v] :
+  strict_mono (is_rank_one_hom v) :=
+classical.some_spec hv.rank_le_one
 
 structure is_discrete (v : valuation R Γ₀) : Prop :=
 (rank_le_one : ∃ f : Γ₀ →* with_zero (multiplicative ℤ), strict_mono f)
 (nontrivial : ∃ r : R, v r ≠ 0 ∧ v r ≠ 1)
 
-variables {G H : Type*} [group G] [group H] (α : G →* H) [decidable_eq (with_zero G)]
+variables {G H : Type*} [group G] [group H] (f : G →* H) [decidable_eq (with_zero G)]
 
 def with_zero.some {x : with_zero G} (hx : x ≠ 0) : G :=
 classical.some (with_zero.ne_zero_iff_exists.mp hx)
@@ -37,14 +45,14 @@ by rw [← with_zero.coe_inj, with_zero.coe_mul, with_zero.some_spec, with_zero.
   with_zero.some_spec]
 
 def with_zero.coe_monoid_hom : with_zero G →* with_zero H := 
-{ to_fun := λ x, if hx : x = 0 then 0 else α (with_zero.some hx),
+{ to_fun := λ x, if hx : x = 0 then 0 else f (with_zero.some hx),
   map_one' := 
   begin
     have h1 : (1 : with_zero G) ≠ 0 := one_ne_zero,
     have h := (classical.some_spec (with_zero.ne_zero_iff_exists.mp h1)),
     rw dif_neg h1,
     simp_rw ← with_zero.coe_one at h ⊢,
-    rw [with_zero.coe_inj, with_zero.some_coe, α.map_one],
+    rw [with_zero.coe_inj, with_zero.some_coe, f.map_one],
   end,
   map_mul' := λ x y,
   begin
@@ -55,7 +63,7 @@ def with_zero.coe_monoid_hom : with_zero G →* with_zero H :=
       { rw [dif_pos hy, mul_zero] }},
     { rw [dif_neg hxy, dif_neg (left_ne_zero_of_mul hxy),
         dif_neg (right_ne_zero_of_mul hxy), ← with_zero.coe_mul,
-        with_zero.coe_inj, ← α.map_mul, with_zero.some_mul hxy] }
+        with_zero.coe_inj, ← f.map_mul, with_zero.some_mul hxy] }
   end }
 
 instance : linear_ordered_comm_monoid_with_zero nnreal := infer_instance
@@ -217,11 +225,207 @@ lemma bar {p : ℕ} [hp : fact p.prime] : is_rank_one (val_p p) :=
   end }
 
 -- Requires choice
-def foo : multiplicative (order_dual (with_top ℝ)) → nnreal := λ x,
+/- def mult_with_top_R_to_nnreal {e : nnreal} (he : 0 ≠ e) :
+  multiplicative (order_dual (with_top ℝ)) → nnreal := λ x,
+begin
+  let y : order_dual (with_top ℝ) := to_add x,
+  by_cases hy : y = ⊥,
+  { exact 0 },
+  { exact e^(classical.some (with_bot.ne_bot_iff_exists.mp hy)) }
+end -/
+
+lemma ne_dual_top_iff_exists {α : Type*} {x : order_dual (with_top α)} :
+  order_dual.of_dual x ≠ ⊤ ↔ ∃ (a : α), ↑a = order_dual.of_dual x :=
+option.ne_none_iff_exists
+
+def mult_with_top_R_to_nnreal {e : nnreal} (he : 0 ≠ e) :
+  multiplicative (order_dual (with_top ℝ)) → nnreal := λ x,
+begin
+  let y : order_dual (with_top ℝ) := to_add x,
+  by_cases hy : order_dual.of_dual y = ⊤,
+  { exact 0 },
+  { exact e^(classical.some (ne_dual_top_iff_exists.mp hy)) }
+end
+
+/- def mult_with_top_R_to_nnreal (e : nnreal) :
+  multiplicative (order_dual (with_top ℝ)) → nnreal := λ x,
 begin
   let y : order_dual (with_top ℝ) := to_add x,
   by_cases hy : y = none,
   { exact 0 },
-  { rw [with_top.none_eq_top, ← ne.def, with_top.ne_top_iff_exists] at hy,
-    exact 2^(classical.some hy), }
+  { rw [← ne.def, with_bot.none_eq_bot, with_bot.ne_bot_iff_exists] at hy,
+    exact e^(classical.some hy) }
+end -/
+
+/- lemma foo (e : nnreal) (r : ℝ) (hr : r ≠ 0) :
+  mult_with_top_R_to_nnreal e (r : order_dual (with_top ℝ)) = r :=
+sorry -/
+
+/- lemma foo (e : nnreal) (r : order_dual (with_top ℝ)) (hr : r ≠ ⊥) :
+  classical.some_spec (with_bot.ne_bot_iff_exists.mp hr)  :=
+begin
+  have r := classical.some (with_bot.ne_bot_iff_exists.mp hr),
+  have h := classical.some_spec (with_bot.ne_bot_iff_exists.mp hr),
+  /- rw ← with_top.coe_eq_coe,
+  exact classical.some_spec (with_bot.ne_bot_iff_exists.mp (with_bot.coe_ne_bot r)), -/
+end -/
+
+/- lemma mult_with_top_apply (r : ℝ) :
+ classical.some (with_bot.ne_bot_iff_exists.mp (with_bot.coe_ne_bot r)) = r :=
+begin
+  rw ← with_bot.coe_eq_coe,
+  exact classical.some_spec (with_bot.ne_bot_iff_exists.mp (with_bot.coe_ne_bot r)),
+end 
+
+lemma mult_with_top_apply (r : ℝ) :
+ classical.some (with_top.ne_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)) = r :=
+begin
+  rw ← with_bot.coe_eq_coe,
+  exact classical.some_spec (with_top.ne_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)),
+end-/
+
+lemma mult_with_top_apply (r : ℝ) :
+ classical.some (ne_dual_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)) = r :=
+begin
+  rw ← with_bot.coe_eq_coe,
+  let s := (order_dual.of_dual (some r) : with_top ℝ),
+  exact classical.some_spec (with_top.ne_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)),
 end
+
+lemma with_top.of_dual_eq_top_iff {α : Type*} {x : order_dual (with_top α)} :
+  order_dual.of_dual x = ⊤ ↔ x = ⊥ := iff.rfl
+
+lemma mult_with_top_R_to_nnreal_strict_mono {e : nnreal} (he0 : 0 < e) (he1 : e < 1) :
+  strict_mono (mult_with_top_R_to_nnreal (ne_of_lt he0)) :=
+begin
+  intros x y hxy,
+  simp only [mult_with_top_R_to_nnreal],
+  by_cases hy  : order_dual.of_dual (y.to_add) = ⊤,
+    { have hxy' : x.to_add < y .to_add := hxy,
+      have hy' : y.to_add = ⊥ := hy,
+      simp only [hy', not_lt_bot] at hxy',
+      exfalso,
+      exact hxy' },
+    { by_cases hx : order_dual.of_dual (x.to_add) = ⊤,
+      { rw [dif_neg hy, dif_pos hx],
+        exact nnreal.rpow_pos he0, },
+      { have hxy' : x.to_add < y .to_add := hxy,
+        rw [dif_neg hx, dif_neg hy],
+        apply nnreal.rpow_lt_rpow_of_exponent_gt he0 he1,
+        have hx' := classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+        rw [← with_top.coe_lt_coe,
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hy)], 
+        exact hxy }}, 
+end
+
+def mult_with_top_R_to_nnreal_monoid_hom {e : nnreal} (he : 0 ≠ e) :
+  multiplicative (order_dual (with_top ℝ)) →* nnreal :=
+{ to_fun   := mult_with_top_R_to_nnreal he,
+  map_one' := begin
+    simp only [mult_with_top_R_to_nnreal, to_add_one],
+    erw [dif_neg (with_bot.coe_ne_bot (0 : ℝ)), mult_with_top_apply (0 : ℝ)],
+    exact nnreal.rpow_zero e,
+  end,
+  map_mul' := λ x y,
+  begin
+    simp only [mult_with_top_R_to_nnreal],
+    by_cases  hx : order_dual.of_dual (x.to_add) = ⊤,
+    { have hxy : order_dual.of_dual ((x * y).to_add) = ⊤,
+      { rw [with_top.of_dual_eq_top_iff, to_add_mul, with_top.of_dual_eq_top_iff.mp hx,
+          with_bot.bot_add] },
+      rw [dif_pos hx, dif_pos hxy, zero_mul] },
+    { by_cases hy : order_dual.of_dual (y.to_add) = ⊤,
+      { have hxy : order_dual.of_dual ((x * y).to_add) = ⊤,
+      { rw [with_top.of_dual_eq_top_iff, to_add_mul, with_top.of_dual_eq_top_iff.mp hy,
+          with_bot.add_bot] },
+        rw [dif_pos hy, dif_pos hxy, mul_zero] },
+      { have hxy : order_dual.of_dual ((x * y).to_add) ≠ ⊤,
+        { rw [ne.def, with_top.of_dual_eq_top_iff, to_add_mul, with_bot.add_eq_bot],
+          exact not_or hx hy, },
+        rw [dif_neg hx, dif_neg hy, dif_neg hxy, ← nnreal.rpow_add (ne.symm he)],
+        apply congr_arg,
+        rw [← with_bot.coe_eq_coe, with_bot.coe_add],
+        rw [classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hy),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hxy), to_add_mul],
+        refl,
+        }},   
+  end, }
+
+  def mult_with_top_R_to_R {e : nnreal} (he : 0 ≠ e) :
+  multiplicative (order_dual (with_top ℝ)) → ℝ := λ x,
+begin
+  let y : order_dual (with_top ℝ) := to_add x,
+  by_cases hy : order_dual.of_dual y = ⊤,
+  { exact 0 },
+  { exact e^(classical.some (ne_dual_top_iff_exists.mp hy)) }
+end
+
+lemma mult_with_top_apply' (r : ℝ) :
+ classical.some (ne_dual_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)) = r :=
+begin
+  rw ← with_bot.coe_eq_coe,
+  let s := (order_dual.of_dual (some r) : with_top ℝ),
+  exact classical.some_spec (with_top.ne_top_iff_exists.mp (@with_top.coe_ne_top ℝ r)),
+end
+
+
+lemma mult_with_top_R_to_R_strict_mono {e : nnreal} (he0 : 0 < e) (he1 : e < 1) :
+  strict_mono (mult_with_top_R_to_nnreal (ne_of_lt he0)) :=
+begin
+  intros x y hxy,
+  simp only [mult_with_top_R_to_nnreal],
+  by_cases hy  : order_dual.of_dual (y.to_add) = ⊤,
+    { have hxy' : x.to_add < y .to_add := hxy,
+      have hy' : y.to_add = ⊥ := hy,
+      simp only [hy', not_lt_bot] at hxy',
+      exfalso,
+      exact hxy' },
+    { by_cases hx : order_dual.of_dual (x.to_add) = ⊤,
+      { rw [dif_neg hy, dif_pos hx],
+        exact nnreal.rpow_pos he0, },
+      { have hxy' : x.to_add < y .to_add := hxy,
+        rw [dif_neg hx, dif_neg hy],
+        apply nnreal.rpow_lt_rpow_of_exponent_gt he0 he1,
+        have hx' := classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+        rw [← with_top.coe_lt_coe,
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hy)], 
+        exact hxy }}, 
+end
+
+def mult_with_top_R_to_R_monoid_hom {e : nnreal} (he : 0 ≠ e) :
+  multiplicative (order_dual (with_top ℝ)) →* nnreal :=
+{ to_fun   := mult_with_top_R_to_nnreal he,
+  map_one' := begin
+    simp only [mult_with_top_R_to_nnreal, to_add_one],
+    erw [dif_neg (with_bot.coe_ne_bot (0 : ℝ)), mult_with_top_apply (0 : ℝ)],
+    exact nnreal.rpow_zero e,
+  end,
+  map_mul' := λ x y,
+  begin
+    simp only [mult_with_top_R_to_nnreal],
+    by_cases  hx : order_dual.of_dual (x.to_add) = ⊤,
+    { have hxy : order_dual.of_dual ((x * y).to_add) = ⊤,
+      { rw [with_top.of_dual_eq_top_iff, to_add_mul, with_top.of_dual_eq_top_iff.mp hx,
+          with_bot.bot_add] },
+      rw [dif_pos hx, dif_pos hxy, zero_mul] },
+    { by_cases hy : order_dual.of_dual (y.to_add) = ⊤,
+      { have hxy : order_dual.of_dual ((x * y).to_add) = ⊤,
+      { rw [with_top.of_dual_eq_top_iff, to_add_mul, with_top.of_dual_eq_top_iff.mp hy,
+          with_bot.add_bot] },
+        rw [dif_pos hy, dif_pos hxy, mul_zero] },
+      { have hxy : order_dual.of_dual ((x * y).to_add) ≠ ⊤,
+        { rw [ne.def, with_top.of_dual_eq_top_iff, to_add_mul, with_bot.add_eq_bot],
+          exact not_or hx hy, },
+        rw [dif_neg hx, dif_neg hy, dif_neg hxy, ← nnreal.rpow_add (ne.symm he)],
+        apply congr_arg,
+        rw [← with_bot.coe_eq_coe, with_bot.coe_add],
+        rw [classical.some_spec (with_bot.ne_bot_iff_exists.mp hx),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hy),
+          classical.some_spec (with_bot.ne_bot_iff_exists.mp hxy), to_add_mul],
+        refl,
+        }},   
+  end, }
+
