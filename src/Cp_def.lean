a8dc23b6348f7feb0e5b4e1b47c23a94564af5c9
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-06-15 22:52:22 +0100

    clean up Cp_def

--- src/Cp_def.lean
+++ src/Cp_def.lean
@@ -1,11 +1,14 @@
 import field_theory.is_alg_closed.algebraic_closure
 import number_theory.padics.padic_numbers
 import topology.metric_space.cau_seq_filter
+import topology.algebra.valued_field
 import spectral_norm
 
 noncomputable theory
 
-variables (p : ℕ)  [fact p.prime]
+open_locale nnreal
+
+variables (p : ℕ) [fact p.prime]
 
 @[reducible] def Q_p_alg : Type* := algebraic_closure ℚ_[p]
 
@@ -15,10 +18,13 @@ algebraic_closure.is_algebraic _
 instance Q_p_alg.normed_field : normed_field (Q_p_alg p) := 
 spectral_norm.normed_field (Q_p_alg.is_algebraic p)
 
-instance Q_p_alg.abv : is_absolute_value (Q_p_alg.normed_field p).norm :=
-normed_field.is_absolute_value
+/- instance Q_p_alg.abv : is_absolute_value (Q_p_alg.normed_field p).norm :=
+normed_field.is_absolute_value -/
 
-def C_p :=  @cau_seq.completion.Cauchy ℝ _ (Q_p_alg p) _ _ (Q_p_alg.abv p)
+/- noncomputable! instance Q_p_alg.valued_field : valued (Q_p_alg p) ℝ≥0 := sorry
+def C_p := uniform_space.completion (Q_p_alg p) -/
+
+def C_p :=  @cau_seq.completion.Cauchy _ _ (Q_p_alg p) _ _ (normed_field.is_absolute_value)
 
 notation `ℂ_[`p`]` := C_p p
 instance : field ℂ_[p] := sorry
@@ -31,6 +37,4 @@ notation `𝓞_ℂ_[`p`]` := C_p_integers p
 
 instance : comm_ring 𝓞_ℂ_[p] := sorry
 
-/- noncomputable! instance Q_p_alg.valued_field : valued (Q_p_alg p) nnreal := 
-sorry -/
 
