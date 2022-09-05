f1f0f571e38e629556c74ebeaa32ca1ba46d9d0b
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-09-05 15:31:56 +0100

    progress on Cp_def

--- src/Cp_def.lean
+++ src/Cp_def.lean
@@ -26,6 +26,10 @@ variables (p : ℕ) [fact p.prime]
 lemma Q_p_alg.is_algebraic : algebra.is_algebraic ℚ_[p] (Q_p_alg p) := 
 algebraic_closure.is_algebraic _
 
+instance : has_coe ℚ_[p] (Q_p_alg p) := ⟨algebra_map ℚ_[p] (Q_p_alg p)⟩
+
+protected lemma coe_eq : (coe : ℚ_[p] → (Q_p_alg p)) = algebra_map ℚ_[p] (Q_p_alg p) := rfl
+
 lemma padic.is_nonarchimedean : is_nonarchimedean (λ (k : ℚ_[p]), ∥k∥₊) :=
 begin
   intros x y,
@@ -52,8 +56,15 @@ begin
   ext x,
   rw [coe_nnnorm], refl,
 end
+
 noncomputable! instance Q_p_alg.valued_field : valued (Q_p_alg p) ℝ≥0 :=
 normed_field.to_valued (Q_p_alg p) (Q_p_alg.is_nonarchimedean p)
+
+lemma Q_p_alg.v_def (x : Q_p_alg p) : 
+  valued.v x = spectral_norm (Q_p_alg.is_algebraic p) x :=
+by { ext, refl }
+
+--lemma Q_p_alg.v_ext (x : ℚ_[p]) : valued.v (x : Q_p_alg p) = ∥ (x : Q_p_alg p)  ∥₊ := rfl
 
 def C_p := uniform_space.completion (Q_p_alg p) 
 
@@ -65,18 +76,30 @@ noncomputable! instance C_p.valued_field : valued (ℂ_[p]) ℝ≥0 := valued.va
 
 instance : has_coe_t (Q_p_alg p) ℂ_[p] := uniform_space.completion.has_coe_t _
 
-lemma C_p.valuation_extends (x : Q_p_alg p) : valued.extension_valuation (x : ℂ_[p]) = valued.v x := 
-begin
-  --simp only [valued.extension_valuation],
-  --change valued.extension (x : ℂ_[p]) = valued.v x,
-  exact valued.extension_extends _,
-end
+lemma C_p.valuation_extends (x : Q_p_alg p) : valued.v (x : ℂ_[p]) = valued.v x := 
+valued.extension_extends _
+
+instance : algebra (Q_p_alg p) ℂ_[p] := 
+uniform_space.completion.algebra' _
+
+example {p : ℕ} [fact p.prime] : ((p : ℚ_[p]) : Q_p_alg p) = (p : Q_p_alg p) :=
+by rw [coe_eq, map_nat_cast]
+
+protected lemma C_p.coe_eq : (coe : (Q_p_alg p) → ℂ_[p]) = algebra_map (Q_p_alg p) ℂ_[p] := rfl
+
+protected lemma C_p.coe_zero : ((0 : Q_p_alg p) : ℂ_[p]) = 0 := rfl
+
 
 example {p : ℕ} [fact p.prime] : ((p : Q_p_alg p) : ℂ_[p]) = (p : ℂ_[p]) :=
+by rw [C_p.coe_eq, map_nat_cast]
+
+lemma Q_p_alg.valuation_p (p : ℕ) [fact p.prime] : valued.v (p : Q_p_alg p) = 1/(p : ℝ≥0) :=
 begin
-  rw ← add_group_with_one.int_cast_of_nat p,
-  
-  sorry--unfold_coes,
+  have hp : (p : Q_p_alg p) = ((p : ℚ_[p]) : Q_p_alg p),
+  { rw [coe_eq, map_nat_cast] },
+  ext,
+  rw [Q_p_alg.v_def, hp, coe_eq, spectral_norm.extends, coe_nnnorm, padic_norm_e.norm_p, one_div, 
+    nonneg.coe_inv, nnreal.coe_nat_cast],
 end
 
 instance : is_rank_one (C_p.valued_field p).v := 
@@ -87,32 +110,53 @@ instance : is_rank_one (C_p.valued_field p).v :=
     haveI hp : nat.prime p := _inst_1.elim,
     have hpv : valued.v (p : ℂ_[p]) = 1/(p : ℝ≥0),
     { have : ((p : Q_p_alg p) : ℂ_[p]) = (p : ℂ_[p]),
-      { sorry },
+      { rw [C_p.coe_eq, map_nat_cast] },
       rw ← this,
-      unfold valued.v, 
       rw C_p.valuation_extends,
-      --simp, 
-      --simp only [valued.extension_valuation],
-      
-      
-      /- simp [monoid_with_zero_hom.has_coe_to_fun.coe_to_fun],
-      simp [valued.extension_extends],
-      set x := (p : Q_p_alg p) with hx,
-      have : valued.extension (x : ℂ_[p]) = valued.v x,
-      { rw valued.extension_extends}, -/
-      --change valued.extension (p : ℂ_[p]) = 1 / (p : ℝ),
-      sorry
-    },
+      exact Q_p_alg.valuation_p p, },
     simp only [hpv,one_div, ne.def, inv_eq_zero, nat.cast_eq_zero, inv_eq_one, nat.cast_eq_one],
     exact ⟨hp.ne_zero, hp.ne_one⟩,
   end }
-
 
 instance : normed_field ℂ_[p] := valued_field.to_normed_field 
   (nat.one_div_cast_pos (nat.prime.pos _inst_1.elim))
   (nat.one_div_cast_lt_one (nat.prime.one_lt _inst_1.elim))
 
-lemma C_p.is_nonarchimedean : is_nonarchimedean (λ (x : ℂ_[p]), ∥x∥₊) := sorry
+lemma C_p.norm_def :
+ (norm : ℂ_[p] → ℝ )= norm_def (nat.one_div_cast_pos (nat.prime.pos _inst_1.elim)) := rfl
+
+lemma C_p.nnnorm_extends (x : Q_p_alg p) : ∥ (x : ℂ_[p]) ∥₊ = ∥ x ∥₊ := 
+begin
+  ext,
+  rw coe_nnnorm, rw coe_nnnorm,
+  by_cases hx : x = 0,
+  { rw [hx, C_p.coe_zero, norm_zero, norm_zero],},
+  { simp only [C_p.norm_def, norm_def, C_p.valuation_extends,
+      mult_with_top_R_to_R_monoid_with_zero_hom, monoid_with_zero_hom.coe_mk, mult_with_top_R_to_R],
+    rw dif_neg,
+    { sorry }, --TODO: is choice of base necessary??
+    { sorry }}
+end
+
+#exit
+
+lemma C_p.is_nonarchimedean : is_nonarchimedean (λ (x : ℂ_[p]), ∥x∥₊) :=
+begin
+  intros x y,
+  apply uniform_space.completion.induction_on₂ x y,
+  { apply is_closed_le,
+    { apply continuous.comp _ continuous_sub,
+      --exact @continuous_nnnorm ℂ_[p] _, TODO: fix diamond
+      sorry, },
+    { apply continuous.max,
+      apply continuous.comp _ (continuous.fst continuous_id),
+      { sorry },
+      apply continuous.comp _ (continuous.snd continuous_id),
+      { sorry }}},
+  { intros a b,
+    simp only [← uniform_space.completion.coe_sub, C_p.nnnorm_extends],
+    exact Q_p_alg.is_nonarchimedean p a b, },
+end
 
 def C_p_integers := metric.closed_ball (0 : ℂ_[p]) 1 --{x : ℂ_[p] // ∥x∥ ≤ 1}
 
