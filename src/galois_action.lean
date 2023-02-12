c1e9aafe7697970ded35e80d9b0fe05830660e0c
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-02-12 23:36:59 +0100

    add comment to galois_action

--- src/galois_action.lean
+++ src/galois_action.lean
@@ -5,18 +5,23 @@ Authors: María Inés de Frutos-Fernández
 -/
 import Cp_def
 
+/-!
+# The `p`-adic complex numbers.
+
+In this file, we show that the  action of the absolute galois of `ℚ_[p]` on `Q_p_alg p` extends to 
+an action on `ℂ_[p]`.
+
+## Tags
+
+p-adic, p adic, padic, galois action, galois
+-/
+
 noncomputable theory
 
 variables {p : ℕ} [fact (nat.prime p)]
 
--- Found by infer_instance
-instance : has_smul ((Q_p_alg p) ≃ₐ[ℚ_[p]] (Q_p_alg p)) (Q_p_alg p) := 
-smul_zero_class.to_has_smul
 
--- Found by infer_instance
-instance : mul_action ((Q_p_alg p) ≃ₐ[ℚ_[p]] (Q_p_alg p)) (Q_p_alg p) := 
-mul_distrib_mul_action.to_mul_action
-
+/-- The elements of the Galois group Gal((Q_p_alg p)/ℚ_[p]) are isometries. -/
 lemma galois_map_isometry (σ : (Q_p_alg p) ≃ₐ[ℚ_[p]] (Q_p_alg p)) : isometry σ :=
 begin
   rw isometry_iff_dist_eq,
@@ -25,8 +30,10 @@ begin
     spectral_norm.aut_isom (Q_p_alg.is_algebraic p) σ (x - y)],
 end
 
+/-- Any `σ` in `Q_p_alg p ≃ₐ[ℚ_[p]] Q_p_alg p` acts uniformly continuously on `Q_p_alg p`. -/
 instance : has_uniform_continuous_const_smul (Q_p_alg p ≃ₐ[ℚ_[p]] Q_p_alg p) (Q_p_alg p) := 
 ⟨λ σ, (galois_map_isometry σ).uniform_continuous⟩
 
+/-- The action of the absolute galois of `ℚ_[p]` on `Q_p_alg p` extends to an action on `ℂ_[p]`.-/
 instance : mul_action ((Q_p_alg p) ≃ₐ[ℚ_[p]] (Q_p_alg p)) (ℂ_[p]) := 
 uniform_space.completion.mul_action ((Q_p_alg p) ≃ₐ[ℚ_[p]] (Q_p_alg p)) (Q_p_alg p)
