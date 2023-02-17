d5cb13c0af9855b8c58dd2266620002621014a23
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-02-17 07:13:31 -0800

    remove import

--- src/minpoly.lean
+++ src/minpoly.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 María Inés de Frutos-Fernández. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández
 -/
-import analysis.normed.ring.seminorm
+
 import field_theory.normal
 
 /-!
@@ -37,7 +37,7 @@ open_locale polynomial
 
 section minpoly
 
-variables {K : Type*} [normed_field K] {L : Type*} [field L] [algebra K L]
+variables {K L : Type*} [field K] [field L] [algebra K L]
 
 namespace adjoin_root
 
